7dc682c87b14bb7da5b086a683e4bf5580a4e568
Vijay Tripathi <vijayt@microsoft.com>
2015-11-25 12:56:33 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-7-jre
+git clone https://github.com/vijaytripathi/vagrant-test.git /home/vagrant/Desktop/vagrant-test
+mv /home/vagrant/Desktop/vagrant-test/test.conf /home/vagrant/.config/upstart/FSE.conf
+reboot
+
